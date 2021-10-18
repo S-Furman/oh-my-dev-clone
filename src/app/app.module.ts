f9d1669c8cbf2accab66d65414adcc41407e097b
S-Furman <szymon.fur@o2.pl>
2021-10-18 13:27:02 +0200

    feat(header): add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SubheaderComponent } from './header/subheader/subheader.component';
+import { MainComponent } from './main/main.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { ArticlesComponent } from './main/articles/articles.component';
+import { ArticleComponent } from './main/articles/article/article.component';
+import { ChipComponent } from './components/chip/chip.component';
+import { CookiesComponent } from './cookies/cookies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubheaderComponent,
+    MainComponent,
+    ArticlesComponent,
+    ArticleComponent,
+    ChipComponent,
+    CookiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
