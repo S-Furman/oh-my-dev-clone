1aefb594c51cbaf969ba2a21148e5419b6d62a67
S-Furman <szymon.fur@o2.pl>
2021-10-23 10:28:04 +0200

    feat(cookies): add closeCookie btn

--- src/app/cookies/cookies.component.ts
+++ src/app/cookies/cookies.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cookies.component.css']
 })
 export class CookiesComponent implements OnInit {
-
+  visible = true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.visible = false;
   }
 
 }
