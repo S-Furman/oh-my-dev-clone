5545d2c77a9ac7392bbf875a545bfa221ee7707a
S-Furman <szymon.fur@o2.pl>
2021-11-30 11:24:49 +0100

    feat(component): add register

---
+++ src/app/main/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { faEyeSlash, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  public faEyeSlash: IconDefinition = faEyeSlash;
+  public isCheckboxChecked: boolean = false;
+  constructor(private authService: AuthService) { }
+  public onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.authUser('signUp', {email, password});
+  }
+  public checkboxHandler(event: boolean) {
+    this.isCheckboxChecked = event;
+  }
+}
