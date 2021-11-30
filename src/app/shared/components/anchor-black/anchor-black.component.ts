12d5e1862fe5f161d6cbbba128379d055ea9bf8d
S-Furman <szymon.fur@o2.pl>
2021-11-30 10:39:05 +0100

    feat(components): add anchor-black

---
+++ src/app/shared/components/anchor-black/anchor-black.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-anchor-black',
+  templateUrl: './anchor-black.component.html',
+  styleUrls: ['./anchor-black.component.scss']
+})
+export class AnchorBlackComponent {
+  @Input() link!: string;
+  constructor() { }
+}
